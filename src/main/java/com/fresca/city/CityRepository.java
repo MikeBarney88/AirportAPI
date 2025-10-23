bf6504cd51e57ed76fc60c3e1068828ee845702f
Joey <jowhiey@gmail.com>
2025-10-23 12:45:33 -0230

    City package completed.
    - CityController is finished and contains GET, POST, PUT, and DELETE routes.
    
    - CityRepository is finished and contains two methods: one to search cities by name and one to search cities by state.
    
    - CityService is completed and is autowired to the CityRepository and the CityController.

--- src/main/java/com/fresca/city/CityRepository.java
+++ src/main/java/com/fresca/city/CityRepository.java
@@ -1,5 +1,13 @@
 package com.fresca.city;
 
-public interface CityRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
+public interface CityRepository extends CrudRepository<City, Long> {
+    //Select Queries
+    City findByName(String name);
+
+    List<City> findByState(String state);
 }
--- src/main/java/com/fresca/city/CityService.java
+++ src/main/java/com/fresca/city/CityService.java
@@ -1,5 +1,61 @@
 package com.fresca.city;
 
-public class CityService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
+@Service
+public class CityService {
+    @Autowired
+    private CityRepository cityRepository;
+
+
+    //GET Operations
+    public List<City> getAllCities() {
+        return (List<City>) cityRepository.findAll();
+    }
+
+    public City getCityById(long id) {
+        Optional<City> cityOptional = cityRepository.findById(id);
+
+        return cityOptional.orElse(null);
+    }
+
+    public List<City> getCitiesByState(String state) {
+        return cityRepository.findByState(state);
+    }
+
+    public City getCityByName(String name) {
+        return cityRepository.findByName(name);
+    }
+
+
+    //POST Operations
+    public City postCreateCity(City newCity) {
+        return cityRepository.save(newCity);
+    }
+
+
+    //PUT Operations
+    public City putUpdateCity(long id, City updatedCity) {
+        Optional<City> cityToUpdateOptional = cityRepository.findById(id);
+
+        if (cityToUpdateOptional.isPresent()) {
+            City cityToUpdate = cityToUpdateOptional.get();
+
+            cityToUpdate.setName(updatedCity.getName());
+            cityToUpdate.setState(updatedCity.getState());
+            cityToUpdate.setPopulation(updatedCity.getPopulation());
+
+            return cityRepository.save(cityToUpdate);
+        }
+
+        return null;
+    }
+
+    //DELETE Operations
+    public void deleteCityById(long id) {
+        cityRepository.deleteById(id);
+    }
 }
