d3f93ad80b4057a9c7ede5a4342367cc06997927
Brandon Pike <Brandon.Pike@keyin.com>
2025-10-23 17:25:30 -0230

    i created my code for the AircraftController.java, i have the Create, stuff, get, update delete etc etc, linking and unlinking airports

--- src/main/java/com/fresca/ApplicationController.java
+++ src/main/java/com/fresca/ApplicationController.java
@@ -1,5 +1,13 @@
 package com.fresca;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ApplicationController {
-    //Main Class
+    // app entrypoint
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationController.class, args);
+    }
 }
+
